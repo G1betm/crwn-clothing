ca8325a7886844e0579eb7d849e780ed1560fe41
Amir Ahmedov <wd@avadirect.com>
2023-06-13 18:59:29 +0500

    added redux thunk

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -4,14 +4,19 @@ import { useSelector } from "react-redux";
 
 import { CategoryContainer, CategoryTitle } from "./category.styles.jsx";
 import ProductCard from "../../components/product-card/product-card.component";
-import { selectCategoriesMap } from "../../store/categories/categories.selector.js";
+import Spinner from "../../components/spinner/spinner.component.jsx";
+import {
+  selectCategoriesMap,
+  selectCategoriesIsLoading,
+} from "../../store/categories/categories.selector.js";
 
 const Category = () => {
   const { category } = useParams();
   const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectCategoriesIsLoading);
 
   const [products, setProducts] = useState(categoriesMap[category]);
-  
+
   useEffect(() => {
     setProducts(categoriesMap[category]);
   }, [category, categoriesMap]);
@@ -19,12 +24,17 @@ const Category = () => {
   return (
     <>
       <CategoryTitle>{category}</CategoryTitle>
-      <CategoryContainer>
-        {products &&
-          products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-      </CategoryContainer>
+
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <CategoryContainer>
+          {products &&
+            products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+        </CategoryContainer>
+      )}
     </>
   );
 };
